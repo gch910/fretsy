0070be62327639ff5b72134db25ad74d9c3ce2ff
Gabriel Harris <gch910@gmail.com>
2021-02-19 11:22:36 -0600

    CSRF protection set up on frontend

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -6,8 +6,7 @@ export async function csrfFetch(url, options = {}) {
   options.headers = options.headers || {};
 
   if (options.method.toUpperCase() !== "GET") {
-    options.headers["Content-Type"] =
-      options.headers["Content-Type"] || "application/json";
+    options.headers["Content-Type"] = options.headers["Content-Type"] || "application/json";
     options.headers["XSRF-Token"] = Cookies.get("XSRF-TOKEN");
   }
 
@@ -16,4 +15,8 @@ export async function csrfFetch(url, options = {}) {
   if (res.status >= 400) throw res;
 
   return res;
+}
+
+export function restoreCSRF() {
+  return csrfFetch('/api/csrf/restore');
 }

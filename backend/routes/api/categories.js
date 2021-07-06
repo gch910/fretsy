6ae85c888b519e77964f4c92df574f5aba4acea3
Gabriel Harris <gch910@gmail.com>
2021-07-06 16:09:16 -0500

    cleaning up categories api routes

--- backend/routes/api/categories.js
+++ backend/routes/api/categories.js
@@ -1,43 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { Category } = require('../../db/models');
-const { Product } = require('../../db/models');
+const { Category } = require("../../db/models");
 
 const asyncHandler = require("express-async-handler");
 
-router.get('/', asyncHandler(async(req, res) => {
-    // const parsedId = parseInt(req.params.categoryId);
-    // const category = await Category.findByPk(parsedId);
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const categories = await Category.findAll();
+
+    res.json({ categories });
+  })
+);
+
+//display random categories on banner1 - categoryValues1 for logged in, categoryValues2 for logged out
+router.get(
+  "/random-categories",
+  asyncHandler(async (req, res) => {
 
     const categories = await Category.findAll();
 
-   console.log(categories)
-  
-    res.json({ categories })
-}));
+    let random = Math.floor(Math.random() * 2);
 
-router.get('/random-categories', asyncHandler(async(req, res) => {
-    // const parsedId = parseInt(req.params.categoryId);
-    // const category = await Category.findByPk(parsedId);
-
-   const categories = await Category.findAll();
-
-   let random = Math.floor(Math.random() * 2);
-
-  if (categories.length > 4) {
-    if (random >= categories.length - 4) {
-      random -= 4;
-    }
-    
-    }
     const categoryValues1 = categories.slice(random, random + 5);
     const categoryValues2 = categories.slice(random, random + 4);
-  
 
-   console.log(categories)
-  
-    res.json({ categoryValues1, categoryValues2 })
-}));
+    console.log("categories lenight", categories.length)
+    console.log("ranodm", random)
+    console.log(categoryValues1)
+
+    res.json({ categoryValues1, categoryValues2 });
+  })
+);
 
 module.exports = router;

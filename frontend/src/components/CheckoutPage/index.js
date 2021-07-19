3f98a4ea53adf7277f2353c0bcfec7307861d994
Gabriel Harris <gch910@gmail.com>
2021-07-18 20:33:44 -0500

    checkout page cleaned up

--- frontend/src/components/CheckoutPage/index.js
+++ frontend/src/components/CheckoutPage/index.js
@@ -1,9 +1,13 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useParams, Link, useHistory } from "react-router-dom";
-import { getCartItems, deleteCartItem, deleteUserCart, addPurchaseHistory } from "../../store/shoppingCart";
+import {
+  getCartItems,
+  deleteCartItem,
+  deleteUserCart,
+  addPurchaseHistory,
+} from "../../store/shoppingCart";
 import "./CheckoutPage.css";
-
 
 const CheckoutPage = () => {
   const { userId } = useParams();
@@ -13,37 +17,31 @@ const CheckoutPage = () => {
 
   const history = useHistory();
 
-  var formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  
+  var formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
   });
-
-  // const cartArray = Object.values(sessionCart);
 
   let totalPrice = 0;
 
   const deleteItemClick = (e) => {
-    // e.preventDefault();
     console.log("inside delete function", e);
-    
+
     dispatch(deleteCartItem(userId, e.target?.id));
-    
   };
 
-  const itemIdArray = sessionCart?.map(item => item?.Product?.id);
+  const itemIdArray = sessionCart?.map((item) => item?.Product?.id);
 
   const makePurchase = (e) => {
     dispatch(deleteUserCart(userId));
-    addPurchaseHistory(userId, itemIdArray)
-    history.push("/complete")
+    addPurchaseHistory(userId, itemIdArray);
+    history.push("/complete");
   };
 
   sessionCart?.forEach((item) => {
     const number = parseInt(item.Product?.price);
     totalPrice += number;
   });
-
 
   useEffect(() => {
     dispatch(getCartItems(userId));
@@ -53,13 +51,24 @@ const CheckoutPage = () => {
     <>
       <div id="checkout-page-grid">
         <h1 id="checkout-h1">Checkout</h1>
-        <h1 id="checkout-price-h1">Your Total: {formatter.format(totalPrice) ? formatter.format(totalPrice) : ""}</h1>
+        <h1 id="checkout-price-h1">
+          Your Total:{" "}
+          {formatter.format(totalPrice) ? formatter.format(totalPrice) : ""}
+        </h1>
         <div id="complete-checkout-div">
-          <button className="no-outline" id="complete-checkout" onClick={makePurchase}>Complete Purchase</button>
+          <button
+            className="no-outline"
+            id="complete-checkout"
+            onClick={makePurchase}
+          >
+            Complete Purchase
+          </button>
         </div>
 
         <Link id="checkout-link" to={`/shopping-cart/${userId}`}>
-          <button className="no-outline" id="back-to-cart">Back To Cart</button>
+          <button className="no-outline" id="back-to-cart">
+            Back To Cart
+          </button>
         </Link>
       </div>
       <div id="cart-h1-div">
